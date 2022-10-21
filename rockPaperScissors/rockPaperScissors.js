526653f01bc3eb124204e1e2e2e5632649d24088
NickWeng18 <nick.weng@outlook.com>
2022-10-21 21:19:22 +1100

    fix game

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -4,16 +4,21 @@ function getComputerChoice() {
     return game[computerChoice];
 }
 
-const playerPrompt = prompt("Choose rock, papers or scissors");
+/*const playerPrompt = prompt("Choose rock, papers or scissors");
 
 const playerSelection = playerPrompt.charAt(0).toUpperCase() + playerPrompt.slice(1);
-const computerSelection = getComputerChoice();
+const computerSelection = getComputerChoice();*/
 
-var win = false;
-var lose = false;
-var draw = false;
+    var win = false;
+    var lose = false;
+    var draw = false;
 
 function playRound(playerSelection, computerSelection){
+    win = false;
+    lose = false;
+    draw = false;
+
+
     if (playerSelection == "Rock") {
         if (computerSelection == "Rock"){
            return draw = true;
@@ -44,46 +49,41 @@ function playRound(playerSelection, computerSelection){
     } else return "Please select a valid input"
 }
 
-playRound(playerSelection, computerSelection);
-
-if (win == true){
-    alert("You win! " + playerSelection + " beats " + computerSelection)
-} else if (false == true){
-    alert("You lose! " + computerSelection +" beats " +playerSelection)
-}else {
-    alert("You draw! you both picked " + playerSelection)
-}
-
 function game(){
     var winCount = 0;
     var loseCount = 0;
 
-    for (let i = 0; i,5; i++){
+    for (let i = 0; i<5; i++){
+        var playerPrompt = prompt("Choose rock, paper or scissors");
         var playerSelection = playerPrompt.charAt(0).toUpperCase() + playerPrompt.slice(1);
         var computerSelection = getComputerChoice();
-        var win = false;
-    var lose = false;
-    var draw = false;
 
-        playRound(playerSelection, computerSelection)
+        playRound(playerSelection, computerSelection);
         if (win == true){
             winCount = winCount + 1;
             alert("You win this round! " + playerSelection + " beats " + computerSelection);
-        } else if (false == true){
+        } else if (lose == true){
             loseCount = loseCount + 1;
             alert("You lose round! " + computerSelection +" beats " + playerSelection);
-        }else {
+        }else if (draw == true){
             alert("You draw this round! you both picked " + playerSelection);
-        }
-
-        if (winCount > loseCount){
-            alert("You win the game! Scores: you:" + winCount + " - computer:" + loseCount);
-        } else if (loseCount > winCount){
-            alert("You lose the game! Scores: you:" + winCount + "- computer:" + loseCount);
-        } else{
-            alert("You draw this game! Scores: you:" + winCount + "- computer:" + loseCount)
+        } 
+        else {
+            alert("Invalid input! Please play round again")
+            i = i - 1;
         }
      }
 
+     if (winCount > loseCount){
+        alert("You win the game! Scores: you: " + winCount + " - computer:" + loseCount);
+    } else if (loseCount > winCount){
+        alert("You lose the game! Scores: you:"  + winCount + "- computer:" + loseCount);
+    } else{
+        alert("You draw this game! Scores: you: " + winCount + "- computer:" + loseCount)
+    }
 
 }
+
+alert("Welcome to Rock, Paper, Scissors! You are playing 5 rounds, highest score wins! Press Ok to proceed. ")
+
+game()
