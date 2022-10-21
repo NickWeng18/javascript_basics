3cf8bab924671fe7ae909fe0db41ed63feeb784b
NickWeng18 <nick.weng@outlook.com>
2022-10-21 20:53:38 +1100

    implement game of 5

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -49,13 +49,41 @@ playRound(playerSelection, computerSelection);
 if (win == true){
     alert("You win! " + playerSelection + " beats " + computerSelection)
 } else if (false == true){
-    alert("You lose! " + computerSelection + " beats " + playerSelection)
+    alert("You lose! " + computerSelection +" beats " +playerSelection)
 }else {
     alert("You draw! you both picked " + playerSelection)
 }
 
-// function game(){
-//     for (let i = 0; i,5; i++){
+function game(){
+    var winCount = 0;
+    var loseCount = 0;
 
-//     }
-// }
+    for (let i = 0; i,5; i++){
+        var playerSelection = playerPrompt.charAt(0).toUpperCase() + playerPrompt.slice(1);
+        var computerSelection = getComputerChoice();
+        var win = false;
+    var lose = false;
+    var draw = false;
+
+        playRound(playerSelection, computerSelection)
+        if (win == true){
+            winCount = winCount + 1;
+            alert("You win this round! " + playerSelection + " beats " + computerSelection);
+        } else if (false == true){
+            loseCount = loseCount + 1;
+            alert("You lose round! " + computerSelection +" beats " + playerSelection);
+        }else {
+            alert("You draw this round! you both picked " + playerSelection);
+        }
+
+        if (winCount > loseCount){
+            alert("You win the game! Scores: you:" + winCount + " - computer:" + loseCount);
+        } else if (loseCount > winCount){
+            alert("You lose the game! Scores: you:" + winCount + "- computer:" + loseCount);
+        } else{
+            alert("You draw this game! Scores: you:" + winCount + "- computer:" + loseCount)
+        }
+     }
+
+
+}
