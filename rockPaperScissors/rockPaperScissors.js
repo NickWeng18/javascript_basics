279a2946e9d40b64695a1f536e5c0d26a05d086a
NickWeng18 <nick.weng@outlook.com>
2022-10-21 20:39:00 +1100

    new syntax

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -9,35 +9,53 @@ const playerPrompt = prompt("Choose rock, papers or scissors");
 const playerSelection = playerPrompt.charAt(0).toUpperCase() + playerPrompt.slice(1);
 const computerSelection = getComputerChoice();
 
+var win = false;
+var lose = false;
+var draw = false;
+
 function playRound(playerSelection, computerSelection){
     if (playerSelection == "Rock") {
         if (computerSelection == "Rock"){
-           return "Draw! you both picked " + computerSelection;
+           return draw = true;
         } else if (computerSelection == "Scissors"){
-            return "You Win! " + playerSelection + " beats " + computerSelection;
+            return win = true;
         } else {
-            return "You Lose! " + computerSelection + " beats " + playerSelection;
+            return lose = true;
         }
 
     } else if (playerSelection == "Scissors") {
         if (computerSelection == "Rock"){
-            return "You Lose! " + computerSelection + " beats " + playerSelection;
+            return lose - true;
         } else if (computerSelection == "Scissors"){
-            return "Draw! you both picked " + computerSelection;
+            return draw = true;
         } else {
-            return "You Win! " + playerSelection + " beats " + computerSelection;
+            return win = true;
         }
 
     } else if (playerSelection == "Paper") {
         if (computerSelection == "Rock"){
-            return "You Win! " + playerSelection + " beats " + computerSelection;
+            return win = true;
         } else if (computerSelection == "Scissors"){
-            return "You Lose! " + computerSelection + " beats " + playerSelection;
+            return lose = true;
         } else {
-            return "Draw! you both picked " + computerSelection;
+            return draw = true;
         }
         
     } else return "Please select a valid input"
 }
 
-alert(playRound(playerSelection, computerSelection))
+playRound(playerSelection, computerSelection);
+
+if (win == true){
+    alert("You win! " + playerSelection + " beats " + computerSelection)
+} else if (false == true){
+    alert("You lose! " + computerSelection + " beats " + playerSelection)
+}else {
+    alert("You draw! you both picked " + playerSelection)
+}
+
+// function game(){
+//     for (let i = 0; i,5; i++){
+
+//     }
+// }
